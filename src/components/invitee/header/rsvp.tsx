b4ccbc56ec14b652e346879f24da6ba44ca1b936
jakub_vala <jakub.vala@betsys.com>
2023-10-01 22:17:30 +0200

    Fix rsvp name

--- src/components/invitee/header/rsvp.tsx
+++ src/components/invitee/header/rsvp.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './rsvp.module.scss';
 const Rsvp: React.FC = () => {
-    return <p className={styles['rsvp']}>RSVP</p>;
+    return <p className={styles['rsvp']}>R.S.V.P.</p>;
 };
 
 export default Rsvp;
