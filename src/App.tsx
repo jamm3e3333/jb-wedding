208587945610e3bb924ab60a863cc6531f923bc4
jakub_vala <jakub.vala@betsys.com>
2023-10-01 22:09:56 +0200

    Replace imgs with small sized imgs

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import CoverWithImage, {
 import LandingAnnouncement from './components/home/landing-announcement';
 import Reception from './components/reception/reception';
 
-const COVER_IMAGE_FILE_PATH = './img/bj_pier_walking.jpeg';
+const COVER_IMAGE_FILE_PATH = './img/bj_pier_walking.webp';
 const GRADIENT_DIRECTION = 'to right';
 const GRADIENT_FROM_COLOR = 'rgba(200, 148, 186, 0.2)';
 const GRADIENT_TO_COLOR = 'rgba(192, 130, 174, 0.3)';
--- src/components/reception/reception-image.tsx
+++ src/components/reception/reception-image.tsx
@@ -1,37 +1,40 @@
-import React, {useEffect, useRef} from "react";
-import {motion, useAnimation, useInView} from "framer-motion";
-import styles from './reception-image.module.scss'
+import React, { useEffect, useRef } from 'react';
+import { motion, useAnimation, useInView } from 'framer-motion';
+import styles from './reception-image.module.scss';
 
-const JAKUB_LIFTING_BRENDA_IMAGE_FILE_PATH = './img/jakub_lifting_brenda.jpeg'
+const JAKUB_LIFTING_BRENDA_IMAGE_FILE_PATH = './img/jakub_lifting_brenda.webp';
 const ReceptionImage: React.FC = () => {
-  const receptionImgRef = useRef(null)
-  const  isInView = useInView(receptionImgRef, { once: true })
-  const animationController = useAnimation()
+    const receptionImgRef = useRef(null);
+    const isInView = useInView(receptionImgRef, { once: true });
+    const animationController = useAnimation();
 
-  useEffect(() => {
+    useEffect(() => {
+        if (isInView) {
+            void animationController.start('visible');
+        }
+    }, [isInView]);
 
-    if (isInView) {
-      void animationController.start('visible')
-    }
-  }, [isInView])
-
-  return <motion.div
-      ref={receptionImgRef}
-      className={styles['image-box']}
-      variants={{
-        hidden: { opacity: 0, y: 70 },
-        visible: { opacity: 1, y: 0 },
-      }}
-      initial="hidden"
-      animate={animationController}
-      transition={{
-        duration: .6,
-        delay: .1,
-      }}
-    >
-      <img src={JAKUB_LIFTING_BRENDA_IMAGE_FILE_PATH} alt="Jakub is lifting Brenda" />
-    </motion.div>
-
-}
+    return (
+        <motion.div
+            ref={receptionImgRef}
+            className={styles['image-box']}
+            variants={{
+                hidden: { opacity: 0, y: 70 },
+                visible: { opacity: 1, y: 0 },
+            }}
+            initial="hidden"
+            animate={animationController}
+            transition={{
+                duration: 0.6,
+                delay: 0.1,
+            }}
+        >
+            <img
+                src={JAKUB_LIFTING_BRENDA_IMAGE_FILE_PATH}
+                alt="Jakub is lifting Brenda"
+            />
+        </motion.div>
+    );
+};
 
 export default ReceptionImage;
