ccb0575c8ef2c4cde93b43c7d692025a9c4d6eed
jakub_vala <jakub.vala@betsys.com>
2023-10-27 22:35:21 +0200

    Refactor fodler names and structure

--- src/components/home/landing-announcement/landing-announcement.tsx
+++ src/components/home/landing-announcement/landing-announcement.tsx
@@ -5,10 +5,17 @@ import CoupleName from './couple-name';
 import EventName from './event-name';
 import SaveTheDateBox from './save-the-date/save-the-date-box';
 import RoutingButton from '../ui/router/routing-button';
+import { useTranslation } from 'react-i18next';
+import '../../i18n/i18n';
+
 const LandingAnnouncement: React.FC = () => {
+    const { t } = useTranslation();
     return (
         <>
-            <RoutingButton name="RVSP" path="/invitee" />
+            <RoutingButton
+                name={t('header.rsvp' as never) as string}
+                path="/invitee"
+            />
             <Box className={styles['announcement']}>
                 <CoupleName />
                 <EventName />
