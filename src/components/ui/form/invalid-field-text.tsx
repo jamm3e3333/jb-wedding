bca436edd940e1f226b6ec9cc03b1b84d491d9a1
jakub_vala <jakub.vala@betsys.com>
2023-10-28 02:17:14 +0200

    Enhance spacing for input, text-area

--- src/components/ui/form/invalid-field-text.tsx
+++ src/components/ui/form/invalid-field-text.tsx
@@ -1,13 +1,15 @@
-import styles from './invalid-field-test.module.scss';
+import styles from './invalid-field-text.module.scss';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-const InvalidFieldText: React.FC<{ invalidTextName: string }> = (props) => {
+const InvalidFieldText: React.FC<{
+    invalidTextName?: string | undefined;
+}> = () => {
     const { t } = useTranslation();
     return (
         <span className={styles['text-invalid']}>
             <i className="fa fa-exclamation-circle"></i>
-            &nbsp; {t('form.invalid' as never)}: {props.invalidTextName}
+            &nbsp; {t('form.invalid' as never)}
         </span>
     );
 };
